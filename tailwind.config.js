07ae1659b8c5a845540003066590996212af2fbe
Rohit kumar <rohit_designer@outlook.com>
2023-06-09 12:58:45 +0200

    tailwind theme edit ,fonts edit html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "420px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      maxWidth: {
+        size: "2rem",
+      },
+      colors: {
+        base: "#f9f7f7",
+        green: "#006D46",
+        black: "#373737",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
