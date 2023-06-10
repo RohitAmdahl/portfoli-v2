4c1c93b4b00e5d69d8d44ee77627f8258be2d3f9
Rohit kumar <rohit_designer@outlook.com>
2023-06-10 16:35:03 +0200

    nav bar update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import TopBar from "./component/TopBar";
 import NavBar from "./component/navBar/NavBar";
 function App() {
-  return (
-    <>
-      <NavBar />
-    </>
-  );
+  return <NavBar />;
 }
 
 export default App;
--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
-import { FaBars } from "react-icons/fa";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Logo from "../../images/logo/logo.png";
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => setToggle(!toggle);
   return (
     <header>
       <div className="flex justify-between items-center px-4 py-4 container max-w-4xl mx-auto  ">
         <div>
           <a href="/">
-            <img src={Logo} alt="logo" className="" />
+            <img src={Logo} alt="logo" />
           </a>
         </div>
+        {/* desktop bar */}
+        <nav className="">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/#projects">Projects</a>
+            </li>
+            <li>
+              <a href="/#skill">Skill</a>
+            </li>
+            <li>
+              <a href="/#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        {/* desktop nav */}
       </div>
+      {/* Toggle button */}
     </header>
   );
 };
