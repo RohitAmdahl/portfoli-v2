1b740dff0770797e74e22ec0aa9438f3aa24216e
Rohit kumar <rohit_designer@outlook.com>
2023-06-12 23:13:53 +0200

    update

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -7,23 +7,23 @@ const Header = () => {
   const handleToggle = () => setToggle(!toggle);
 
   return (
-    <div className=" py-4  font-poppins mx-auto flex justify-center items-center pt-14  ">
-      <header className=" z-50 fixed max-w-4xl mx-auto container bg-white flex justify-between items-center shadow-lg p-5 m-3 rounded-full capitalize ">
-        <a href="#home" className="logo text-2xl font-bold text-accent">
+    <div className=" mx-auto  flex items-center justify-center py-4 pt-14 font-poppins   ">
+      <header className=" container fixed z-50 m-3 mx-auto flex max-w-4xl items-center justify-between rounded-full bg-white p-5 capitalize shadow-lg ">
+        <a href="#home" className="logo text-accent text-2xl font-bold">
           <img src={Logo} alt="logo" className=" max-w-size" />
         </a>
 
         {/* Desktop Nav */}
         <nav className="hidden md:block  ">
           <ul className="flex justify-between px-4  ">
-            <li className="px-4 text-green  hover:underline lg:border-r-2  md:border-r-2 ">
+            <li className="px-4 text-green  hover:underline md:border-r-2  lg:border-r-2 ">
               <a href="#home">Home</a>
             </li>
 
-            <li className="px-4 hover:text-green hover:underline  lg:border-r-2 md:border-r-2">
+            <li className="px-4 hover:text-green hover:underline  md:border-r-2 lg:border-r-2">
               <a href="#skills">skills</a>
             </li>
-            <li className="px-4 hover:text-green hover:underline lg:border-r-2 md:border-r-2 ">
+            <li className="px-4 hover:text-green hover:underline md:border-r-2 lg:border-r-2 ">
               <a href="#projects">Projects</a>
             </li>
 
@@ -34,24 +34,24 @@ const Header = () => {
         </nav>
 
         {/* Mobile Nav */}
-        <div className="flex justify-center items-center">
+        <div className="flex items-center justify-center">
           <nav
             className={
               !toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0  "
             }
           >
-            <ul className="container max-w-lg mx-auto ">
-              <li className="py-4 flex justify-center items-center hover:underline ">
+            <ul className="container mx-auto max-w-lg ">
+              <li className="flex items-center justify-center py-4 hover:underline ">
                 <a href="#home">Home</a>
               </li>
 
-              <li className="py-4 flex justify-center items-center hover:underline ">
+              <li className="flex items-center justify-center py-4 hover:underline ">
                 <a href="#skills">Skills</a>
               </li>
-              <li className="py-4 flex justify-center items-center hover:underline ">
+              <li className="flex items-center justify-center py-4 hover:underline ">
                 <a href="#projects">Projects</a>
               </li>
-              <li className="py-4 flex justify-center items-center hover:underline ">
+              <li className="flex items-center justify-center py-4 hover:underline ">
                 <a href="#contact">Contact</a>
               </li>
             </ul>
