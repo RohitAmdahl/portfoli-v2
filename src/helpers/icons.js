5b32430704ed305cccef0e04fee6eb205b26c1a5
Rohit kumar <rohit_designer@outlook.com>
2023-06-12 19:41:00 +0200

    added icons list

---
+++ src/helpers/icons.js
@@ -0,0 +1,71 @@
+import Img1 from "../images/icons/html.png";
+import Img2 from "../images/icons/css.png";
+import Img3 from "../images/icons/sass.png";
+import Img4 from "../images/icons/bootstrap.png";
+import Img5 from "../images/icons/logos_javascript.png";
+import Img6 from "../images/icons/skill-icons_react-dark.png";
+import Img7 from "../images/icons/threejs.png";
+import Img8 from "../images/icons/figma.png";
+import Img9 from "../images/icons/bi_git.png";
+import Img10 from "../images/icons/wordpress.png";
+import Img11 from "../images/icons/skill-icons_supabase-dark.png";
+
+const skillIcons = [
+  {
+    id: 1,
+    name: "html5",
+    image: Img1,
+  },
+  {
+    id: 2,
+    name: "css",
+    image: Img2,
+  },
+  {
+    id: 3,
+    name: "sass",
+    image: Img3,
+  },
+  {
+    id: 4,
+    name: "bootstrap",
+    image: Img4,
+  },
+  {
+    id: 5,
+    name: "Javascript",
+    image: Img5,
+  },
+  {
+    id: 6,
+    name: "react.js",
+    image: Img6,
+  },
+  {
+    id: 7,
+    name: "three.js",
+    image: Img7,
+  },
+  {
+    id: 8,
+    name: "figma",
+    image: Img8,
+  },
+  {
+    id: 9,
+    name: "git",
+    image: Img9,
+  },
+  {
+    id: 10,
+    name: "figma",
+    image: Img10,
+  },
+  {
+    id: 11,
+    name: "figma",
+    image: Img11,
+  },
+];
+
+export default skillIcons;
