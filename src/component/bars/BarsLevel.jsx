48388651e55a4be08b11201f4629d6a93ebed153
Rohit kumar <rohit_designer@outlook.com>
2023-06-13 23:43:17 +0200

    update

---
+++ src/component/bars/BarsLevel.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const BarsLevel = () => {
+  return (
+    <div className="container mx-auto max-w-lg px-10 pb-3 pt-3">
+      <div className="container mx-auto flex max-w-lg items-center gap-20 py-14 lg:flex-row">
+        <div className="my-auto flex w-full flex-col gap-3">
+          <h2 className="font-medium capitalize text-indigo-700">
+            Figma, XD, PS.
+          </h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200 ">
+            <div className="h-1.5 w-full rounded-md bg-green"></div>
+          </div>
+
+          <h2 className="font-medium capitalize text-indigo-700">HTML</h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-full rounded-md bg-green"></div>
+          </div>
+
+          <h2 className="font-medium capitalize text-indigo-800">
+            css and sass, tailwind css, material ui.
+          </h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-5/6 rounded-md bg-green"></div>
+          </div>
+
+          <h2 className="font-medium capitalize text-indigo-800">bootstrap</h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-5/6 rounded-md bg-green"></div>
+          </div>
+
+          <h2 className="font-medium capitalize text-indigo-800">Javascript</h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-4/6 rounded-md bg-green"></div>
+          </div>
+          <h2 className="font-medium capitalize text-indigo-800">react.js</h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-4/6 rounded-md bg-green"></div>
+          </div>
+
+          <h2 className="font-medium capitalize text-indigo-800">
+            git and Github
+          </h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-5/6 rounded-md bg-green"></div>
+          </div>
+
+          <h2 className="font-medium capitalize text-indigo-800">three.js</h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-1/5 rounded-md bg-green"></div>
+          </div>
+          <h2 className="font-medium capitalize text-indigo-800">
+            Backend open source supabase
+          </h2>
+          <div className="h-1.5 w-full rounded-md bg-gray-200">
+            <div className="h-1.5 w-5/6 rounded-md bg-green"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarsLevel;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MySkills from "../component/skills/Skills";
 import SocialMedia from "../component/stickyIconBar/SocialMedia";
+import BarsLevel from "../component/bars/BarsLevel";
 const Skills = () => {
   return (
     <div>
       <SocialMedia />
       <MySkills />
+      <BarsLevel />
     </div>
   );
 };
