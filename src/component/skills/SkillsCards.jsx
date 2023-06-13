9d3804da78535c1709288aa8dfb1a1818172b0c3
Rohit kumar <rohit_designer@outlook.com>
2023-06-13 22:41:14 +0200

    update

--- src/component/skills/SkillsCards.jsx
+++ src/component/skills/SkillsCards.jsx
@@ -6,8 +6,8 @@ const SkillsCards = ({ icons }) => {
   return (
     <>
       <div className="pb-2 pt-1">
-        <div className="flex max-w-size flex-col rounded-lg bg-white px-1  shadow-lg ">
-          <img src={image} alt={name} className=" max-w-size p-4" />
+        <div className=" flex max-w-size flex-col rounded-lg bg-white px-1  shadow-lg ">
+          <img src={image} alt={name} className="p-4 lg:p-8" />
           <div className="text-center">
             <p className="pt-1 text-sm capitalize md:text-lg lg:text-lg">
               {name}
