229d65aa4cf7a6eea05571ccd430e362ed3e84bd
Rohit kumar <rohit_designer@outlook.com>
2023-06-13 22:44:01 +0200

    update

--- src/component/skills/SkillsCards.jsx
+++ src/component/skills/SkillsCards.jsx
@@ -7,9 +7,9 @@ const SkillsCards = ({ icons }) => {
     <>
       <div className="pb-2 pt-1">
         <div className=" flex max-w-size flex-col rounded-lg bg-white px-1  shadow-lg ">
-          <img src={image} alt={name} className="p-4 lg:p-8" />
+          <img src={image} alt={name} className="p-4 md:p-10 lg:p-8" />
           <div className="text-center">
-            <p className="pt-1 text-sm capitalize md:text-lg lg:text-lg">
+            <p className="pb-2 pt-1 text-sm capitalize md:text-lg lg:text-lg">
               {name}
             </p>
           </div>
