7535adc6f0451c5f689ea518e561114ba030c275
Rohit kumar <rohit_designer@outlook.com>
2023-06-17 02:39:48 +0200

    update

--- src/component/projects/ProjectCards.jsx
+++ src/component/projects/ProjectCards.jsx
@@ -4,13 +4,13 @@ const ProjectCards = ({ project }) => {
   const { image, title } = project;
   return (
     <>
-      <div className="dark:bg-gray-800p-4 container m-3 mx-auto max-w-lg rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700">
-        <img className=" rounded-t-lg" src={image} alt={title} />
+      <div className="dark:bg-gray-800p-4 text-sans container m-3 mx-auto max-w-lg  rounded-lg  shadow dark:border-gray-700 ">
         <div className="container mx-auto p-5">
           <h5 className="mb-2 text-2xl font-bold capitalize tracking-tight text-black dark:text-white">
             {title}
           </h5>
         </div>
+        <img className="rounded-t-lg" src={image} alt={title} />
       </div>
     </>
   );
