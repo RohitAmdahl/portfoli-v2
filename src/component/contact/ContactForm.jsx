2db73dded82b124fca6c7986d62cc01489099905
Rohit kumar <rohit_designer@outlook.com>
2023-06-20 17:03:58 +0200

    working on form

--- src/component/contact/ContactForm.jsx
+++ src/component/contact/ContactForm.jsx
@@ -1,84 +1,66 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-const schema = yup
-  .object({
-    firstName: yup
-      .string()
-      .min(3, "Your first name should be at least 3 characters.")
-      .required("Please enter your first name"),
-    email: yup
-      .string()
-      .email("email should be valid email.")
-      .required("Please write your valid email"),
-    message: yup
-      .string()
-      .min(3, "Your message should be at least 3 characters.")
-      .required("Please write your subject"),
-  })
-  .required();
-
-function onSubmit(data) {
-  console.log(data);
-}
-
-// const submitForm = (data) => {
-//   console.log(data);
-// };
-
+// import { useForm } from "react-hook-form";
+import { useForm, ValidationError } from "@formspree/react";
 const ContactForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+  const [state, handleSubmit] = useForm("myyarvel");
+  if (state.succeeded) {
+    return (
+      <>
+        <p className="mx-auto py-3 text-center font-serif text-2xl font-bold">
+          Thanks for your Message!
+        </p>
+      </>
+    );
+  }
   return (
-    <div className=" container mx-auto max-w-lg p-2">
+    <div className=" container mx-auto flex  max-w-xl flex-col p-2">
       <div>
-        <h1 className="py-3 text-center text-2xl font-bold">Contact Me </h1>
+        <h1 className="py-3 text-center text-2xl font-bold">Contact Me</h1>
       </div>
       <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col justify-center"
+        action="https://formspree.io/f/myyarvel"
+        onSubmit={handleSubmit}
+        className="grid grid-cols-1"
       >
+        <label htmlFor="name">Name</label>
         <input
           className="border-primary m-1 rounded-md border p-2 "
-          type="text"
-          {...register("firstName")}
-          placeholder="First Name..."
+          id="name"
+          type="name"
+          name="name"
+          required
+          autoComplete="off"
         />
-        <p className=" font-pins font-medium text-red-400 ">
-          {errors.firstName?.message}
-        </p>
-
+        <label htmlFor="email">Email Address</label>
         <input
           className="border-primary m-1 rounded-md border p-2 "
-          type="text"
-          {...register("email")}
-          placeholder="Email..."
+          id="email"
+          type="email"
+          name="email"
+          required
+          autoComplete="off"
         />
-        <p className=" font-pins font-medium text-red-400 ">
-          {errors.email?.message}
-        </p>
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <label htmlFor="Message">Message</label>
         <textarea
           className="border-primary m-1 mb-4 rounded-md border p-2 "
-          {...register("message")}
-          placeholder="Write your message..."
-          cols="30"
-          rows="5"
-        ></textarea>
-        <p className=" font-pins font-medium text-red-400 ">
-          {errors.message?.message}
-        </p>
-        <input
-          type="submit"
-          id="submit"
-          className="flex cursor-pointer  items-center  justify-center gap-2 rounded-full bg-green  px-2 py-2 text-lg capitalize  text-white shadow-md shadow-black duration-100 ease-out hover:bg-black hover:text-white"
+          id="message"
+          name="message"
+          required
+          autoComplete="off"
         />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button
+          className=" container mx-auto mt-4 max-w-sm cursor-pointer rounded-full bg-green px-2 py-2 text-lg text-white shadow-md shadow-black duration-150 ease-in-out hover:bg-black"
+          type="submit"
+          disabled={state.submitting}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
