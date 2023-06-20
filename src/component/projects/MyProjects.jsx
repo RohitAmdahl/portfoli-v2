eda6225008960fedf9be416be93cd351f932b9f8
Rohit kumar <rohit_designer@outlook.com>
2023-06-20 19:02:33 +0200

    update

--- src/component/projects/MyProjects.jsx
+++ src/component/projects/MyProjects.jsx
@@ -7,7 +7,13 @@ const MyProjects = () => {
     <div className="mt-2 px-2 py-4 lg:mt-4 lg:px-4 lg:py-8 ">
       <div className="container mx-auto grid max-w-lg grid-cols-1  gap-4 md:max-w-3xl md:grid-cols-1 md:gap-5 lg:max-w-5xl lg:grid-cols-2 lg:gap-8 ">
         {projectData.map((project) => {
-          return <ProjectCards key={project.id} project={project} />;
+          return (
+            <ProjectCards
+              key={project.id}
+              project={project}
+              className="overflow-hidden"
+            />
+          );
         })}
       </div>
     </div>
