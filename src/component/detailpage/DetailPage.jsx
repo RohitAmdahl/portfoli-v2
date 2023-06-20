d0c4c76dbb001ed49c20dbd201411fc6383bcba4
Rohit kumar <rohit_designer@outlook.com>
2023-06-20 23:09:27 +0200

    edit styles

--- src/component/detailpage/DetailPage.jsx
+++ src/component/detailpage/DetailPage.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import projectData from "../../helpers/projects";
-
+import logo from "../../images/logo/logo.png";
 const ProjectDetail = () => {
   const { id } = useParams();
   const data = projectData[id];
 
-  console.log(data);
   return (
     <>
       <div className="container mx-auto max-w-4xl pt-8 text-center font-serif  text-3xl">
-        <h1 className="text-bold pb-10 text-4xl capitalize ">{data.title}</h1>
+        <h1 className="text-bold pb-10 text-4xl capitalize">{data.title}</h1>
         <div>
           <img
             src={data.image}
@@ -20,13 +19,15 @@ const ProjectDetail = () => {
         </div>
       </div>
       <div className="container mx-auto max-w-xl p-4 text-start">
-        <p className="py-5 font-serif text-4xl capitalize">description</p>
+        <p className="p-6 py-5 font-serif text-4xl capitalize">description</p>
 
-        <p className="text-lg"> {data.description} </p>
+        <p className="p-6 pl-10 pr-2 text-lg"> {data.description} </p>
       </div>
       <div className="container mx-auto max-w-xl p-4 text-start">
-        <p className="py-5 font-serif text-4xl capitalize">Learning area </p>
-        <p className="text-lg"> {data.learning} </p>
+        <p className=" px-6 py-5 font-serif text-4xl capitalize">
+          "the things I learned"
+        </p>
+        <p className=" py-3 pl-10 pr-2 text-lg"> {data.learning} </p>
       </div>
     </>
   );
