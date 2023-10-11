4c65412430d35b96c6be77cb8579d5126822a84e
rohitAmdahl <rohit_designer@outlook.com>
2023-10-11 12:34:59 +0200

    removed extra pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
 import Layout from "./layout/Layout";
-import ProjectDetail from "./pages/ProjectDetail";
 import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDetail />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
