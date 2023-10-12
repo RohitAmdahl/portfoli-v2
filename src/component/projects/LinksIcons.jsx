868db0b13902157d03f16f57051d6b4e98bcd888
Rohit kumar Amdahl <91061651+RohitAmdahl@users.noreply.github.com>
2023-10-12 14:33:57 +0200

    Update LinksIcons.jsx

--- src/component/projects/LinksIcons.jsx
+++ src/component/projects/LinksIcons.jsx
@@ -35,14 +35,4 @@ const LinksIcons = ({ linkProject }) => {
 };
 
 export default LinksIcons;
-//   return (
-//     <div className="grid grid-cols-3 items-center">
-//       <div className="flex justify-center px-2 py-2 ">
-//         <a href="">github</a>
-//       </div>
-//       <div className=" flex justify-center px-2 py-2 ">
-//         <a href="">website</a>
-//       </div>
-//       <div className="   bg-red-200 "></div>
-//     </div>
-//   );
+
